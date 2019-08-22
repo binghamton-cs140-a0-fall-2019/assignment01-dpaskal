ccc5705daaea447f80abc5944d5383ce52e05735
Daniel Paskalev <dpaskal1@binghamton.edu>
2019-08-22 11:28:10 -0400

    added tester classes

---
+++ assignment01/ComputerTester.java
@@ -0,0 +1,11 @@
+package assignment01;
+public class ComputerTester {
+	public static void main(String[] args) {
+	//TODO make 4 different Computer objects with different 
+	//combinations of disk size (two more than a terabyte and two less)
+	//and for each make one an SSD drive and the other not.
+	//Then call System.out.println(comp); for each "comp" object
+	//and observe that there are small differences in how the computer
+	//information prints out.
+	}
+}

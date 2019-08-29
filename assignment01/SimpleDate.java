5a08b21721c6d6159334d63a3c40c771537df5bb
Daniel Paskalev <dpaskal1@binghamton.edu>
2019-08-28 20:28:30 -0400

    Yer

--- assignment01/SimpleDate.java
+++ assignment01/SimpleDate.java
@@ -1,1 +1,38 @@
 package assignment01;
+
+public class SimpleDate{
+    public int year;
+    public int month;
+    public int day;
+
+    // public SimpleDate(int yr, int mnth, int day){
+    //     this.year = yr;
+    //     this.month = mnth;
+    //     this.date = day;
+    // }
+
+    public static SimpleDate of(int yr, int m, int d){
+        var returnValue = new SimpleDate();
+        returnValue.year = yr;
+        returnValue.month = m;
+        returnValue.day = d;
+        return returnValue;
+    }
+
+    public boolean before(SimpleDate other){
+        boolean returnValue = false;
+        if (this.year == other.year && this.month == other.month && this.day == other.day){
+            returnValue = false;
+        }
+        else if (this.year == other.year && this.month == other.month){
+            returnValue = this.day < other.day;
+        }
+        else if (this.year == other.year){
+            returnValue = this.month < other.month;
+        }
+        else {
+            returnValue = this.year < other.year;
+        }
+        return returnValue;
+    }
+}
